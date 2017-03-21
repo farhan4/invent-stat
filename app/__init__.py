c4be731476b2a9d728a240eb2e5c3903e1a0c5a0
farhan4 <baigfarhan99@gmail.com>
2017-03-21 20:01:14 +0530

    Added homepage and other webpages

--- app/__init__.py
+++ app/__init__.py
@@ -22,14 +22,16 @@ def login():
          attempted_username = request.form['username']
          attempted_password = request.form['password']
 
-         data = c.execute("SELECT * FROM users WHERE username = (%s)",
-                              request.form['username'])
-         data = c.fetchone()
 
          if attempted_username == "admin" and attempted_password == "admin":
              return render_template('home.html')
 
-         elif attempted_username == data[0] and attempted_password == data[1]:
+         data = c.execute("SELECT * FROM users WHERE username = (%s)",
+                              request.form['username'])
+         data = c.fetchone()
+
+
+         if attempted_username == data[0] and attempted_password == data[1]:
              return render_template('home.html')
          else:
              error="Invalid Credentials , try again."
@@ -38,8 +40,29 @@ def login():
     except Exception as e:
         # flash(e)
         error = "Invalid credentials, try again."
-        return render_template("login.html", error=error)
+        return render_template('login.html', error=error)
 
+@app.route('/stock/')
+def print_stock():
+        c, conn = connection()
+        cursor = c.execute('SELECT * FROM stock')
+        item = [dict(pid=row[0],
+                        pname=row[1],
+                        quantity=row[2],
+                        price=row[3]) for row in c.fetchall()]
+        return render_template('stock.html', items=item)
+
+
+@app.route('/home/')
+def home():
+  return render_template('home.html')
+
+@app.route('/bill/')
+def bill():
+    c, conn = connection()
+    cursor = c.execute('SELECT pname FROM product')
+    item = c.fetchall()
+    return render_template('bill.html', items= item)
 
 if __name__ == '__main__':
     app.run(debug = True)
