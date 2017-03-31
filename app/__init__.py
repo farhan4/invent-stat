d7f4933d1f91aaaeef5d41bf6f34b14c959a4c19
farhan4 <baigfarhan99@gmail.com>
2017-03-31 22:16:24 +0530

    Updated Bill Generation

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import *
 import MySQLdb
 import gc
@@ -57,12 +58,62 @@ def print_stock():
 def home():
   return render_template('home.html')
 
-@app.route('/bill/')
+@app.route('/bill/',methods = ["GET","POST"])
 def bill():
-    c, conn = connection()
-    cursor = c.execute('SELECT pname FROM product')
-    item = c.fetchall()
-    return render_template('bill.html', items= item)
 
+   # try:
+      c, conn = connection()
+      cursor = c.execute('SELECT * FROM stock')
+      item = [dict(pname=row[1], price=row[3]) for row in c.fetchall()]
+      if request.method == "POST":
+
+        i = 0
+        name = "123"
+        productarr = []
+        qtyarr = []
+        pricearr =[]
+
+        while 'product_name' + str(i) in request.form:
+             n = "product_name" + str(i)
+             q = "qty" + str(i)
+             p = "price" + str(i)
+
+             name = request.form[n]
+             qty = request.form[q]
+             price = request.form[p]
+
+             productarr.append(name)
+             qtyarr.append(qty)
+             pricearr.append(price)
+             i=i+1
+
+        sql=[]
+
+        date = request.form['date']
+        total =  request.form['total']
+
+        data = c.execute('SELECT * FROM bill ORDER BY bill_no DESC LIMIT 1')
+        data = c.fetchone()
+        bill_number = data[0]
+        bill_number = bill_number + 1
+
+
+        sql.append("INSERT INTO bill VALUES('%s','%s','%s')" %(bill_number,date,total))
+        cursor = c.execute(sql[0])
+        conn.commit()
+
+        for j in range(1,len(productarr)+1) :
+             pname = productarr[j - 1]
+             qty = qtyarr[j - 1]
+             price = pricearr[j - 1]
+             sql.append("INSERT INTO items VALUES('%s','%s','%s','%s')" %(bill_number,pname,qty,price))
+             cursor = c.execute(sql[j])
+             conn.commit()
+        return render_template('success.html')
+      return render_template('bill.html', items=item)
+
+   # except Exception as e:
+    #    print(e)
+    #    return render_template('error.html')
 if __name__ == '__main__':
     app.run(debug = True)
